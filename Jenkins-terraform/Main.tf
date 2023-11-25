4593ff5e32df99310f312c63048d70c68ca00336
Bhushan Sangale <137383680+Bhushan0151@users.noreply.github.com>
2023-11-25 19:31:32 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -60,9 +60,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
+  ami                    = "ami-0e83be366243f524a"
   instance_type          = "t2.large"
-  key_name               = "Mumbai"
+  key_name               = "Ohio-pem"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
@@ -72,6 +72,6 @@ resource "aws_instance" "web" {
   }
 
   root_block_device {
-    volume_size = 30
+    volume_size = 40
   }
 }
